df78573634b2d2d98ae6d91716e4fde530ec07b7
Jhy <jhy@red.home>
2020-04-01 15:27:23 +0200

    restitution

--- script.py
+++ script.py
@@ -7,6 +7,12 @@ from pymongo import MongoClient
 from flask_pymongo import PyMongo 
 import mysql.connector
 import random
+from pandas import read_csv
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.neural_network import MLPClassifier
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import train_test_split
 
 #endregion#
 
@@ -61,13 +67,13 @@ def formulaire_post():
         if "H" in sexe:
             titre = 'Mr'
         elif "F" in sexe:
-            titre ="Ms"
+            titre ="Mme"
         else :
             titre = "The Appache Helicoptere"
     elif "M" in sexe:
         titre = 'Mr'
     elif "F" in sexe:
-        titre ="Ms"
+        titre ="Mme"
     else :
         titre = "The Appache Helicoptere"
 
@@ -89,7 +95,7 @@ def formulaire_post_NOSQL():
     if sexe == "Homme":
         titre = "Mr"
     elif sexe =="Femme":
-        titre = "Ms"
+        titre = "Mme"
     else :
         titre = "The Appache Helicoptere"
     
@@ -100,33 +106,28 @@ def formulaire_post_NOSQL():
 
     pseudo_take = mongo.db.users.posts.find_one({"_id": pseudo})
 
-    if pseudo_take is None :
-        reg_id = mongo.db.users.insert(
-            {
-                '_id': pseudo,
-                'Genre': titre,
-                'Nom': nom,
-                'Prenom': prenom,
-            })
-    else : 
-        pseudo = pseudo +'0'
-        reg_id = mongo.db.users.insert(
-            {
-                '_id': pseudo,
-                'Genre': titre,
-                'Nom': nom,
-                'Prenom': prenom,
-            })
-    
+    try :
+        if pseudo_take is None :
+            reg_id = mongo.db.users.insert_one(
+                {
+                    '_id': pseudo,
+                    'Genre': sexe,
+                    'Nom': nom,
+                    'Prenom': prenom,
+                })
+
+    except :
+        return "Ce nom d'utilisateur est déja pris !"
+
     return render_template("formulaire database.html",titre=titre, prenom=prenom,nom=nom,pseudo=pseudo)
     client.close()
 
 @app.route('/formulaire-SQL')
 def formulaire_SQL():
 #TODO inserer version SQL
+    myConnection = mysql.connector.connect( host=hostname, user=username, passwd=password, db=database ) #SQL
+    myConnection.close()
     return render_template("formulaire database.html")
-    #myConnection = mysql.connector.connect( host=hostname, user=username, passwd=password, db=database ) #SQL
-    #myConnection.close()
 
 @app.route('/formulaire-SQL',methods=['POST'])
 def formulaire_post_SQL():
@@ -135,7 +136,7 @@ def formulaire_post_SQL():
     if sexe == "Homme":
         titre = "Mr"
     elif sexe =="Femme":
-        titre = "Ms"
+        titre = "Mme"
     else :
         titre = "The Appache Helicoptere"
     
@@ -145,19 +146,73 @@ def formulaire_post_SQL():
 
     return render_template("formulaire database.html",titre=titre, prenom=prenom,nom=nom,pseudo=pseudo)
 
+@app.route('/database',methods=['GET'])
+def database_list():
+    database = db.users.find({})
+    liste = []
+
+    for x in database :
+        liste.append(x)
+    
+    databaselight = db.users.distinct('_id')
+    listlight = []
+
+    for x in databaselight :
+        listlight.append(x)
+
+    client.close()
+    return render_template("database.html",database=liste,databaselight=listlight)
+
+@app.route('/liste_utilisateur',methods=['GET'])
+def utilisateurs_list():
+    database = db.users.distinct('_id')
+    userlist = []
+
+    for x in database :
+        userlist.append(x)
+    
+    client.close()
+    return render_template("database.html",database=userlist, databaselight="")
+
+@app.route('/statistique',methods=['GET'])
+def statistique():
+    longueur = len(list(db.users.distinct('_id')))
+
+    cpt_homme = 0
+    cpt_femme = 0
+
+    cursor = db.users.find({})
+    for document in cursor: 
+        if document['Genre'] == "Homme":
+            cpt_homme += 1
+        else :
+            cpt_femme += 1
+    try :
+        parite_homme = round(((longueur / cpt_homme) *100))
+    except :
+        parite_homme = 0
+    try :
+        parite_femme = round(((longueur / cpt_femme) *100))
+    except :
+        parite_femme = 0
+    parite = (parite_homme,parite_femme)
+    
+    client.close()
+    return render_template("stats.html",longueur=longueur,homme=parite_homme,femme=parite_femme)
+
+
+@app.route('/mnist')
+def mnist():
+    online_path="http://www.math.univ-toulouse.fr/~besse/Wikistat/data/"
+    data=read_csv(online_path+"mnist_train.csv",header=None)
+    Y=data.iloc[:,784] 
+    X= data.drop(data.columns[[784]], axis=1,inplace=True)
+    X_train, X_test, y_train, y_test = train_test_split(X,Y,random=5)
+
+    return "nope, c'était pas pret"
 #endregion#
 
 #region# Launch APP
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-"""
-    research = users.find_one({'_id':reg_id})
-    output = ({
-            'Nom':nom,
-            'Prenom':prenom,
-            'Genre':titre,
-            'Pseudo':pseudo
-            })
-"""
